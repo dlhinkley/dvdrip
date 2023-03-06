9a2334207ef8107448af7f361c2f4417a9e82cb3
Duane Hinkley <{ID}+{username}@users.noreply.github.com>
2023-03-05 16:25:33 -0800

    fix

--- s3sync.sh
+++ s3sync.sh
@@ -3,7 +3,9 @@
 find /mnt/usb* -type f | while read f
 do
     echo "Copy '$f'"
+    #FILE=`echo $f | sed -e 's/ /\\\\ /g' -e 's/-/\\\\-/g'`
     CACHE=`echo '$f' | md5sum | cut -d ' ' -f 1`
-    s3cmd -rv --cache-file=/home/pi/.s3md5/$CACHE sync \'$f\' s3://infuse-videos
+    #s3cmd -rv --cache-file=/home/pi/.s3md5/$CACHE sync "'$f'" s3://infuse-videos
+    find "$f" -type f -exec s3cmd -rv --no-delete-removed  --cache-file=/home/pi/.s3md5/$CACHE sync "{}" s3://infuse-videos  \;
 done
 
